ba4c4c0bf5718703bae5f16b9549e0e867b4391d
srinivas.kool <31861607+srinivaskool@users.noreply.github.com>
2023-10-18 02:01:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+import OpenAI from 'openai';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import fetch from 'node-fetch';
+
+const app = express();
+const port = 3004;
+
+app.use(cors()); 
+
+app.use(bodyParser.json());
+
+app.use(express.json());
+
+app.get('/hi', (req, res) => {
+    try {
+        res.status(200).json({ message: 'Hi bro, Srinivas here' });
+    } catch (error) {
+        // Handle errors
+        console.error(error);
+        res.status(500).json({ error: error });
+    }
+});
+
+
+app.post('/forward', async (req, res) => {
+  try {
+    const targetUrl = 'https://api.openai.com/v1/chat/completions'; // Replace with the URL you want to send the POST request to
+
+    const response = await fetch(targetUrl, {
+      method: 'POST',
+      body: JSON.stringify({
+            stream: req.body.stream,
+            model: "gpt-3.5-turbo",
+            max_tokens: 1000,
+            temperature: 0.7,
+            n: 1,
+            messages: req.body.messages,
+      }),
+      headers: { 'Content-Type': 'application/json' , 'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`},
+    });
+
+    if (!response.ok) {
+      throw new Error(`Request failed with status: ${response.status}`);
+    }
+    console.log("sending data", response);
+
+    // Stream the response back to the client
+    response.body.pipe(res);
+
+  } catch (error) {
+    // Handle errors appropriately
+    res.status(500).json({ error: error });
+  }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
